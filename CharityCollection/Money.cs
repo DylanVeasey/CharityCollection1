754e3043549104b0f7cfdaeddf08d8e48430380a
Dylan Veasey <43819824+DylanVeasey@users.noreply.github.com>
2020-10-07 16:45:11 +0100

    Finished Project

---
+++ CharityCollection/Money.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityCollection
+{
+    class Money
+    {
+        public int amountCollected;
+
+        public void GetAmount()
+        {
+            Console.WriteLine("How much money did you raise?");
+            amountCollected = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ CharityCollection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int totalRaised; 
+
+            Money Person1 = new Money();
+            Money Person2 = new Money();
+            Money Person3 = new Money();
+
+            Person1.GetAmount();
+            Person2.GetAmount();
+            Person3.GetAmount();
+
+            totalRaised = Person1.amountCollected + Person2.amountCollected + Person3.amountCollected;
+
+            if(totalRaised > 1000)
+            {
+                Console.WriteLine("A total of " + totalRaised + " was raised.");
+                Console.WriteLine("This will be doubled to " + totalRaised * 2);
+            }
+            else
+            {
+                Console.WriteLine("A total of " + totalRaised + " was raised.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
